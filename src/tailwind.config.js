b50e8a658d9c2b6a215fe91b637fd89ddf694e37
jfleezfsvoai <jfleezfsvoai@gmail.com>
2026-03-27 11:20:32 +0800

    update tailwind

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class', // <--- 加入了这一行，这是解决黑色卡片的关键！
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
